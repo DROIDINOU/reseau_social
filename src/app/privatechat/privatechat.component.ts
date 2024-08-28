68f5ff8bc0c5d4b9bc8ab25d6c8423561f383208
DROIDINOU <76789720+DROIDINOU@users.noreply.github.com>
2024-08-29 00:33:20 +0200

    chat css and html

--- src/app/privatechat/privatechat.component.ts
+++ src/app/privatechat/privatechat.component.ts
@@ -1,20 +1,25 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CommunicationService } from '../communication.service'; // Assurez-vous que le chemin est correct
 import { Subscription } from 'rxjs';
+import { DatePipe } from '@angular/common';
+
 
 @Component({
   selector: 'app-privatechat',
   templateUrl: './privatechat.component.html',
-  styleUrls: ['./privatechat.component.scss']
+  styleUrls: ['./privatechat.component.scss'],  
+  providers: [DatePipe]
+
 })
 export class PrivatechatComponent implements OnInit, OnDestroy {
+  currentTime: Date;
   public messages: any[] = [];
   public messageText: string = '';
   private user1: string = 'marc';  // Remplacez par l'utilisateur courant
   private user2: string = 'nar';  // Remplacez par l'utilisateur avec qui vous discutez
   private chatSubscription: Subscription | null = null;
 
-  constructor(private chatService: CommunicationService) {}
+  constructor(private chatService: CommunicationService, private datePipe: DatePipe) {this.currentTime = new Date();}
 
   ngOnInit(): void {
     this.chatService.connect(this.user1, this.user2);
@@ -25,6 +30,12 @@ export class PrivatechatComponent implements OnInit, OnDestroy {
       console.log('Message reçu:', parsedMessage); // Vérifiez la structure des messages reçus
       this.messages.push(parsedMessage);
     });
+  }
+  
+  formatTime(date: Date): string {
+    const formattedDate = this.datePipe.transform(date, 'HH:mm');
+    // Assurez-vous que formattedDate n'est pas null
+    return formattedDate !== null ? formattedDate : '';
   }
 
   ngOnDestroy(): void {
